d5b582039d6881b5730d45afe4e1d2b4597bea71
Courtney <courtney.s.morse@gmail.com>
2018-03-24 14:53:49 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,7 +3,9 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(height, width) {
+  var table = document.getElementById("pixel_canvas");
+  var grid = '';
 
 // Your code goes here!
 
