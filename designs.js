67cf336c6db9af7b9e01b609d54c03d6c9ae4585
Courtney <courtney.s.morse@gmail.com>
2018-04-01 21:45:04 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -8,7 +8,14 @@ const ctx = canvas.getContext(“2d”);
 ctx.fillStyle = “red”;
 ctx.fillRect(10, 10, 150, 150);
 
+function drawLine(x1, y1, x2, y2){
+   ctx.beginPath();
+   ctx.moveTo(x1, y1);
+   ctx.lineTo(x2, y2);
+   ctx.stroke();
+}
 
+drawLine(15, 175, 250, 350);
 // Your code goes here!
 
 }
